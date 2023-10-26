e13bea85cb2542df079f9faacfb4181d00a025cd
Barbora Černá <barboracerna267@gmail.com>
2023-10-26 21:17:01 +0200

    final

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+const myname=prompt('Zadejte křestní jméno bez diakritiky:');
+const surname=prompt('Zadejte příjmení bez diakritiky:');
+const domain= 'fit.cvut.cz'
+const atsign= '@'
+
+myname.trim();
+surname.trim();
+myname.slice(0, 2);
+surname.slice(0, 4);
+
+
+document.body.innerHTML =
+  `<h2>Váš e-mail:</h2>` + surname.slice(0, 5) + myname.slice(0, 3) + atsign + domain;
